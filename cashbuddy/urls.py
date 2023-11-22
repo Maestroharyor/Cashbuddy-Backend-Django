0e56694cab579fb38ae804c8438a00cc143a97d8
Maestro Haryor <ayomide.odewale1@gmail.com>
2023-11-22 02:38:48 +0100

    CHanges

--- cashbuddy/urls.py
+++ cashbuddy/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import include, path
 from .views import welcome_to_cashbuddy
 
@@ -23,3 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('cashbuddy.routes.routes_url')),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
