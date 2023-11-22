8d1b31f29878dc118c19f5fa139b0d7ac8739a0c
Maestro Haryor <ayomide.odewale1@gmail.com>
2023-11-22 02:40:10 +0100

    Changes

--- cashbuddy/urls.py
+++ cashbuddy/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.conf import settings
+# from django.conf import settings
 from django.urls import include, path
 from .views import welcome_to_cashbuddy
 
@@ -26,6 +26,6 @@ urlpatterns = [
     path('api/', include('cashbuddy.routes.routes_url')),
 ]
 
-if settings.DEBUG:
-    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
